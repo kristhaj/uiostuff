9fb1f80e4451a983fab0a582df1e8b6c3d9a9415
kristhaj <hajemkristoffer@gmail.com>
2026-01-16 13:31:29 +0100

    add optional functionality for user input

--- IN1010/old/prac2/GameOfLife.java
+++ IN1010/old/prac2/GameOfLife.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class GameOfLife {
     
     public static void main(String[] args) {
-        Verden verden = new Verden(8, 12);
-        System.out.println("Startverden:");
+        //Ta input fra bruker for å definere en startverden
+        Scanner brukerInput = new Scanner(System.in);
+        System.out.println("Angi ønsket antall rader:");
+        // rad input
+        int rader = brukerInput.nextInt();
+        System.out.println("Angi ønsket antall kolonner:");
+        // kolonne input
+        int kolonner = brukerInput.nextInt();
+
+        Verden verden = new Verden(rader, kolonner);
+        System.out.printf("Startverden (%d x %d):", rader, kolonner);
         verden.tegn();
-        for (int i=1; i<=3; i++) {
+
+        System.out.println("Angi hvor mange generasjoner som skal simuleres:");
+        // generasjoner input
+        int generasjoner = brukerInput.nextInt();
+        for (int i=1; i<=generasjoner; i++) {
             verden.oppdatering();
             System.out.printf("\n%d generasjon(er) simulert.\nVerden:", i);
             verden.tegn();
